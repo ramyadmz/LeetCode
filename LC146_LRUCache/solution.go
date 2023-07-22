e4f9b07df10bc0ff90b6020a3cae6175bda78a0e
ramyadmz <ramyadmz@gmail.com>
2023-07-22 12:14:23 -0700

    add / remove node

--- LC146_LRUCache/solution.go
+++ LC146_LRUCache/solution.go
@@ -17,6 +17,38 @@ func Constructor(capacity int) LRUCache {
 	}
 }
 
+// AddNode creates a new node with the given key and value, and adds it to the cache.
+func (this *LRUCache) addNode(key, value int) *Node {
+	node := &Node{
+		key:   key,
+		value: value,
+		next:  this.tail,
+	}
+	if this.head == nil {
+		this.head = node
+	}
+	if this.tail != nil {
+		this.tail.prev = node
+	}
+	this.tail = node
+	return node
+}
+
+// RemoveNode removes the given node from the cache.
+func (this *LRUCache) removeNode(node *Node) {
+	if node.prev != nil {
+		node.prev.next = node.next
+	} else {
+		this.tail = node.next
+	}
+
+	if node.next != nil {
+		node.next.prev = node.prev
+	} else {
+		this.head = node.prev
+	}
+}
+
 func (this *LRUCache) Get(key int) int {
 
 }
