c70487156739269daf305a38e617b29350ef2b6e
ramyadmz <ramyadmz@gmail.com>
2023-07-22 12:13:25 -0700

    add type and constructor

--- LC146_LRUCache/solution.go
+++ LC146_LRUCache/solution.go
@@ -1,8 +1,20 @@
+package lrucache
+
+type Node struct {
+	key, value int
+	next, prev *Node
+}
 type LRUCache struct {
+	keyToNode  map[int]*Node
+	head, tail *Node
+	capacity   int
 }
 
 func Constructor(capacity int) LRUCache {
-
+	return LRUCache{
+		keyToNode: make(map[int]*Node),
+		capacity:  capacity,
+	}
 }
 
 func (this *LRUCache) Get(key int) int {
@@ -12,4 +24,3 @@ func (this *LRUCache) Get(key int) int {
 func (this *LRUCache) Put(key int, value int) {
 
 }
-
